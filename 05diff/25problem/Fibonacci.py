6758296e02272c7a7a371ac1b5dba2c5e7049521
ericlau00 <sharkguy418@gmail.com>
2019-05-18 02:08:13 -0400

    python iterative solution

--- 05diff/25problem/Fibonacci.py
+++ 05diff/25problem/Fibonacci.py
@@ -1,16 +1,22 @@
+import time 
 def toFib(index):
-    phi = (1 + 5 ** 0.5) / 2 
-    psi = (1 - 5 ** 0.5) / 2 
-    return (phi ** index - psi ** index) / 5 ** 0.5 
+    sub2 = 0
+    sub1 = 1
+    for i in range(1,index):
+        storage = sub1
+        sub1 += sub2
+        sub2 = storage 
+    return sub1 
      
 def solution():
     place = 1
-    while not digitCount(toFib(place), 50 ): 
-        print toFib(place)
+    while not digitCount(toFib(place), 1000 ): 
         place += 1
     print place, toFib(place)
    
 def digitCount(number, digits):
-    return len(str(number)) - 2 == digits 
+    return len(str(number)) == digits 
     
+start = time.time()
 solution()
+print time.time() - start 
