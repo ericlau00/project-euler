8157988c3924455ab5b0ffc73903d636ec40808a
ericlau00 <sharkguy418@gmail.com>
2019-09-15 18:26:19 -0400

    fix results but still not working

--- 20diff/145inc/reversibleNumbers.py
+++ 20diff/145inc/reversibleNumbers.py
@@ -1,11 +1,13 @@
-def reversibleNumbers():
+import time;
+
+def reversibleNumbers(limit):
     reversible = []
-    for i in range(1,1000):
+    for i in range(1,limit):
         if not i in reversible:
             if isReversible(i):
                 reversible.append(i)
                 reversible.append(int(str(i)[::-1]))
-    print reversible
+    print(len(reversible))
     
 def isReversible(num):
     if "0" in str(num)[-1]:
@@ -16,17 +18,19 @@ def isReversible(num):
         if int(digit) % 2 == 0:
             return False
     return True 
-    
-reversibleNumbers()
+
+start = time.time()
+reversibleNumbers(300000)
+print(time.time() - start, "seconds")
 
 ##0 10 ten 
 ##20 100 hundred 
 ##120 1000 thousand 
 ##720 10000 ten thousand 
 ##720 100000 hundred thousand 
-##4320 200000 two hundred thousand 
-##7920 300000 three hundred thousand 
-##10620 400000 four hundred thousand 
+##7920 200000 two hundred thousand 
+##13320 300000 three hundred thousand 
+##16920 400000 four hundred thousand 
 ##1000000 million 
 ##10000000 ten million 
 ##100000000 hundred million 
